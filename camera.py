ba6f544ffcc0aca1cb7da5dae4553fddf9997822
Gideon Tong <gideon@gideontong.com>
2023-08-29 13:30:28 -0700

    Print statements

--- camera.py
+++ camera.py
@@ -30,6 +30,7 @@ if __name__ == '__main__':
     lcd.fill((0,0,0))
     cap = cv2.VideoCapture(URL)
     ret, frame = cap.read()
+    print(frame.shape[2])
     image_surface = pygame.image.frombuffer(frame.tobytes(), frame.shape[1::-1], 'BGR')
     image_surface = pygame.transform.scale(image_surface, (200, 113))
     lcd.blit(image_surface, (20, 190))
@@ -41,4 +42,4 @@ if __name__ == '__main__':
     pygame.display.update()
     pygame.mouse.set_visible(False)
     pygame.display.update()
-    sleep(10)
+    sleep(5)
