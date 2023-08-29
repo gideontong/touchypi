6e871229f40911e0a9ccb07e8c0d4bdfb4905bae
Gideon Tong <gideon@gideontong.com>
2023-08-29 00:52:42 -0700

    It's coming

--- camera.py
+++ camera.py
@@ -11,22 +11,29 @@ from touchypi.const import RUNNING_ON_PI
 
 from requests import request
 
-URL = 'https://www.kcscout.net/TransSuite.VCS.CameraSnapshots/K070WBC-01.jpg'
+import json
+import cv2
+
+with open('secrets.json') as fp:
+    secrets = json.load(fp)
+
+URL = f'rtsp://{secrets["username"]}:{secrets["password"]}@{secrets["ip"]}/stream2'
 
 if RUNNING_ON_PI:
     print("Pi detected, setting display out to framebuffer")
     os.environ['SDL_FBDEV'] = '/dev/fb1'
 
-print(os.environ['SDL_FBDEV'])
 
 if __name__ == '__main__':
     pygame.init()
     lcd = pygame.display.set_mode((240, 320))
     lcd.fill((0,0,0))
-    r = request('GET', URL)
-    img = io.BytesIO(r.content)
-    print(r.status_code)
-    image_surface = pygame.image.load(img)
+    # r = request('GET', URL)
+    # img = io.BytesIO(r.content)
+    # print(r.status_code)
+    cap = cv2.VideoCapture(URL)
+    ret, frame = cap.read()
+    image_surface = pygame.image.frombuffer(frame.tostring(), frame.shape[1::-1], 'BGR')
     image_surface = pygame.transform.scale(image_surface, (200, 113))
     lcd.blit(image_surface, (20, 190))
     pygame.display.update()
