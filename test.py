ae6c30748be5fd4c470bf0504f89353d6db8da42
Gideon Tong <gideon@gideontong.com>
2023-08-28 23:59:57 -0700

    Environment variables

--- test.py
+++ test.py
@@ -7,7 +7,7 @@ import pygame
 import os
 from time import sleep
 
-os.putenv('SDL_FBDEV', '/dev/fb0')
+os.environ['SDL_FBDEV'] = '/dev/fb1'
 print(os.environ['SDL_FBDEV'])
 pygame.init()
 lcd = pygame.display.set_mode((320, 240))
