eea781518ddae3463f486535c659c0bf86fbef62
Gideon Tong <gideon@gideontong.com>
2023-09-14 23:09:05 -0700

    Keep last click

--- touchypi/events/__init__.py
+++ touchypi/events/__init__.py
@@ -11,8 +11,9 @@ from typing import Union
 
 from touchypi.events.constants import DELAY_ALLOWED_NS
 
+last_click = None
 last_click_time = time.time_ns()
 
 def touch_cycle_clock() -> Union[None, tuple]:
     if time.time_ns() - last_click_time < DELAY_ALLOWED_NS:
-        return None
+        return last_click
